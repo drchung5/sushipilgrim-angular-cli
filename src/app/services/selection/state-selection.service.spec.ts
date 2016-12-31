45bb13894ae7fd5ed29cc5c3f0a4447e997d674b
David Chung <dchung@dchung.com>
2016-12-31 16:17:57 -0600

    Update documentation on testing Observable

--- src/app/services/selection/state-selection.service.spec.ts
+++ src/app/services/selection/state-selection.service.spec.ts
@@ -10,9 +10,12 @@ describe('StateSelectionService', ()=>{
   })
 
   // The pattern here is to:
-  //  1. subscribe to the Observable
-  //  2. set its value
-  //  3. handle the subscription callback
+  //  1. inject the service
+  //  2. subscribe to the Observable
+  //  3. set its value
+  //  4. handle the subscription callback
+  //  5. define the 'expect' clause
+  //  6. call done()
   it('Should set state', done => {
     inject([ StateSelectionService ], stateSelectionService => {
 
