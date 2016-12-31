f22128371208bf0a523e40eeedb5669838f232df
David Chung <dchung@dchung.com>
2016-12-31 16:09:31 -0600

    Added test to validate handling of Observable in StateSelectionService

--- src/app/services/selection/selection.service.spec.ts
+++ src/app/services/selection/selection.service.spec.ts
@@ -1,20 +1,31 @@
-// TODO build async test
+import { TestBed, inject  } from '@angular/core/testing'
+import { StateSelectionService } from './selection.service'
 
-// import { TestBed, injectAsync  } from '@angular/core/testing'
-// import { StateSelectionService } from './selection.service'
-//
-// describe('StateSelectionService', ()=>{
-//
-//   beforeEach(()=>{
-//     TestBed.configureTestingModule({
-//       declarations: [ StateSelectionService ]
-//     })
-//   })
-//
-//   it( 'Should set state',
-//     injectAsync([StateSelectionService], (stateSelectionService)=> {
-//       expect( stateSelectionService.setState( "ABC" ) ).
-//     })
-//   )
-//
-// })
+describe('StateSelectionService', ()=>{
+
+  beforeEach(()=>{
+    TestBed.configureTestingModule({
+      providers: [ StateSelectionService ]
+    })
+  })
+
+  // The pattern here is to:
+  //  1. subscribe to the Observable
+  //  2. set its value
+  //  3. handle the subscription callback
+  it('Should set state', done => {
+    inject([ StateSelectionService ], stateSelectionService => {
+
+      stateSelectionService.selected$.subscribe(
+        state => {
+          expect( state ).toBe('AZ')
+          done()
+        }
+      )
+
+      stateSelectionService.setState('AZ')
+
+    })() // inject returns a function: call it here
+  })
+
+})
